a54a59409e8650e96b3d02d3d7a847916a28c68d
shreyavaidy <shreyavaidy@gmail.com>
2016-03-05 17:50:22 +0530

    func2

---
+++ func2.js
@@ -0,0 +1,12 @@
+
+console . log (" The future says :" , future ());
+
+function future () {
+return " We STILL have no flying cars .";
+}
+
+console.log("   Currently :", deb());
+
+function deb() {
+return " We are trying to debate it out.";
+}
