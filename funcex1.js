3edcc84fc3b36d20d53d5e4083a277322ff24728
shreyavaidy <shreyavaidy@gmail.com>
2016-03-06 15:42:26 +0530

    funcex1

---
+++ funcex1.js
@@ -0,0 +1,11 @@
+function min(num1, num2)
+{   console.log("The smaller number between "+num1+" and "+num2+" is :");
+    var small=0;
+    if(num1<num2)
+        small=num1;
+    else
+        small=num2;
+    console.log(small);
+};
+
+min(15,10);
